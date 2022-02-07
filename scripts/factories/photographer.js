53c871237632d85c785d9a2f7bffde40ac1adb65
Sirius <ahmedhouari942@gmail.com>
2022-02-07 22:38:26 +0100

    ajout du html & css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,23 @@
 function photographerFactory(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `./FishEye-Photos/Sample-Photos/All-Photo/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        const lien = document.createElement('a');
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const contentParagraph = document.createElement('div')
+        contentParagraph.setAttribute("class", "description-photograph")
+        const paragraphVille = document.createElement('p')
+        article.appendChild(lien);
+        lien.appendChild(img);
+        lien.appendChild(h2);
+        lien.appendChild(contentParagraph);
+        contentParagraph.appendChild(paragraphVille);
         return (article);
     }
     return { name, picture, getUserCardDOM }
