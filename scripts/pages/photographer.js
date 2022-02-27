18180332ab016840de6b43267dbd52de85af78de
Sirius <ahmedhouari942@gmail.com>
2022-02-27 21:10:04 +0100

    ajout de media photographers.js photo/video

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,13 +1,10 @@
 //Mettre le code JavaScript lié à la page photographer.html
-
+// creation d'une nouvelle page avec les id
 const idPhotograph = window.location.search.split("?").join("");
-const photographHeader = document.querySelector(".photograph-header")
+const photographHeader = document.querySelector(".photograph-header");
 const contentImage = document.querySelector(".content-image");
-
 console.log(idPhotograph);
 
-
-let thePhoto = [];
 let userMedia = [];
 
 const fetchCall = async () => {
@@ -17,8 +14,6 @@ const fetchCall = async () => {
       userMedia = promise.media;
       userPhotograph = promise.photographers;
     });
-   
-  
 };
 fetchCall();
 
@@ -26,10 +21,10 @@ const test = async () => {
   await fetchCall();
   console.log(userMedia);
   console.log(userPhotograph);
-  for (let i = 0; i < 6 ; i++) {
-      if(userPhotograph[i].id == idPhotograph){
-          console.log(userPhotograph[i]);
-          photographHeader.innerHTML = `
+  for (let i = 0; i < 6; i++) {
+    if (userPhotograph[i].id == idPhotograph) {
+      // console.log(userPhotograph[i]);
+      photographHeader.innerHTML = `
           <div class="name-photograph">
           <h2>${userPhotograph[i].name}</h2>
           <h3>${userPhotograph[i].city}, ${userPhotograph[i].country}</h3>
@@ -39,42 +34,150 @@ const test = async () => {
           Contactez-moi
         </button>
         <img
-          src="./FishEye-Photos/Sample-Photos/All-Photo/${userPhotograph[i].portrait}"
+          src="./FishEye-Photos/Sample-Photos/Photographers_ID_Photos/${userPhotograph[i].portrait}"
           alt="Photo-${userPhotograph[i].name}"
-        />`
-      }
-   
-    
+        />`;
+    }
   }
-    
-    
 };
 
 test();
 
 const mediaFunction = async () => {
   await fetchCall();
-  for(let i = 0; i < 59; i++){
-    if(userMedia[i].photographerId == idPhotograph){
-      console.log(userMedia[i]);
-      contentImage.innerHTML = `
-      <article>
-          <img
-            src="FishEye-Photos/Sample-Photos/Mimi/Animals_Rainbow.jpg"
-            alt="Arc-en-ciel"
-          />
-          <div class="content-like">
-            <h3>Arc-en-ciel</h3>
-            <div class="number-icon">
-              <span class="number-like" aria-label="numberlikes">12</span>
-              <span class="icon-like" aria-label="likes"
-                ><i class="fas fa-heart"></i
-              ></span>
-            </div>
-          </div>
-        </article>`
-    }
-  }
+  const result = userMedia.filter(
+    (list) => list.photographerId == idPhotograph
+  );
+  const resultPhoto = result.filter((list) => list.image);
+  const resultVideo = result.filter((list) => list.video);
+  const divVideo = document.createElement("article");
+  console.log(result);
+  console.log(resultPhoto);
+  console.log(resultVideo);
+
+  contentImage.innerHTML = resultPhoto
+    .map(
+      (user) =>
+        `
+  <article>
+  <img
+    src="FishEye-Photos/Sample-Photos/${user.photographerId}/${user.image}"
+    alt="${user.title}" class="image-replace"
+  />
+  <div class="content-like">
+    <h3>${user.title}</h3>
+    <div class="number-icon">
+      <span class="number-like" aria-label="numberlikes">${user.likes}</span>
+      <span class="icon-like" aria-label="likes"
+        ><i class="fas fa-heart"></i
+      ></span>
+    </div>
+  </div>
+</article>
+
+  `
+    )
+    .join("");
+  contentImage.appendChild(divVideo);
+  console.log(divVideo);
+  divVideo.classList.add("video")
+  divVideo.innerHTML = resultVideo.map(
+    (user) =>
+      `
   
-}
+  <video controls="controls">
+  <source src="FishEye-Photos/Sample-Photos/${user.photographerId}/${user.video}">
+  </video>
+  <div class="content-like">
+    <h3>${user.title}</h3>
+    <div class="number-icon">
+      <span class="number-like" aria-label="numberlikes">${user.likes}</span>
+      <span class="icon-like" aria-label="likes"
+        ><i class="fas fa-heart"></i
+      ></span>
+    </div>
+  </div>
+
+  `
+  );
+};
+
 mediaFunction();
+// <video src="FishEye-Photos/Sample-Photos/${user.photographerId}/${user.video}"></video>
+
+// userMedia.forEach(element =>  console.log(element.photographerId))
+// const found = userMedia.find(element => element.photographerId == idPhotograph )
+// console.log(found);
+
+// contentImage.innerHTML = `
+// <h2> ${found.photographerId} </h2>`
+
+//   contentImage.innerHTML = resultVideo.map((user) =>
+//   `<article>
+//   <video src="FishEye-Photos/Sample-Photos/${user.photographerId}/${user.video}"
+//     alt="${user.title}" class="image-replace">
+//     </video>
+//   <div class="content-like">
+//     <h3>${user.title}</h3>
+//     <div class="number-icon">
+//       <span class="number-like" aria-label="numberlikes">${user.likes}</span>
+//       <span class="icon-like" aria-label="likes"
+//         ><i class="fas fa-heart"></i
+//       ></span>
+//     </div>
+//   </div>
+// </article>
+//   `)
+
+// for(let i = 0; i < userMedia.length; i++){
+//   if(userMedia[i].photographerId == idPhotograph){
+
+//     console.log(userMedia[i].title);
+
+//     contentImage.innerHTML =  userMedia.map((user) => `
+//     <article>
+//         <img
+//           src="FishEye-Photos/Sample-Photos/243/Animals_Rainbow.jpg"
+//           alt="Arc-en-ciel"
+//         />
+//         <div class="content-like">
+//           <h3>${user.title}</h3>
+//           <div class="number-icon">
+//             <span class="number-like" aria-label="numberlikes">12</span>
+//             <span class="icon-like" aria-label="likes"
+//               ><i class="fas fa-heart"></i
+//             ></span>
+//           </div>
+//         </div>
+//       </article>` )
+
+//   }
+
+// }
+
+//  await userMedia.map((user) => { if(user.photographerId == idPhotograph){
+//     console.log(user.title);
+//     const resultat = userMedia.find( data => data.photographerId == idPhotograph);
+//     console.log(resultat);
+//     console.log(resultat);
+//    contentImage.innerHTML = `
+//        <article>
+//            <img
+//              src="FishEye-Photos/Sample-Photos/243/Animals_Rainbow.jpg"
+//              alt="Arc-en-ciel"
+//            />
+//            <div class="content-like">
+//              <h3>${resultat.title}</h3>
+//              <div class="number-icon">
+//                <span class="number-like" aria-label="numberlikes">12</span>
+//                <span class="icon-like" aria-label="likes"
+//                  ><i class="fas fa-heart"></i
+//                ></span>
+//              </div>
+//            </div>
+//          </article>`
+//   }
+
+//   })
+
+
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,17 +1,6 @@
 // form contact
-const formValidation = document.querySelector("#formvalidation");
-// all Input
-const prenomInput = document.querySelector("#first");
-const nameInput = document.querySelector("#last");
-const emailInput = document.querySelector("#email");
-const yourMessage = document.querySelector("#message");
-// message error
-const prenomError = document.querySelector(".prenom-error");
-const nameError = document.querySelector(".name-error");
-const emailError = document.querySelector(".message-adresse");
-const messageError = document.querySelector(".message-error");
-// regex
-let regexmail = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+const modalContact = document.querySelector("#contact_modal");
+console.log(modalContact);
 
 function displayModal() {
   const modal = document.getElementById("contact_modal");
@@ -23,47 +12,141 @@ function closeModal() {
   modal.style.display = "none";
 }
 
-formValidation.addEventListener("submit", (e) => {
-// first name
-  if (prenomInput.value.length <= 1) {
-    prenomError.classList.add("display-block");
-    prenomInput.classList.add("border-invalid");
-    e.preventDefault();
-  } else {
-    prenomError.classList.remove("display-block");
-    prenomInput.classList.remove("border-invalid");
-    prenomInput.classList.add("border-valid");
-  }
-// name
-  if (nameInput.value.length <= 1) {
-    nameError.classList.add("display-block");
-    nameInput.classList.add("border-invalid");
-    e.preventDefault();
-  } else {
-    nameError.classList.remove("display-block");
-    nameInput.classList.remove("border-invalid");
-    nameInput.classList.add("border-valid");
-  }
-// email
-  if (emailInput.value.match(regexmail)) {
-    emailError.classList.remove("display-block");
-    emailInput.classList.add("border-valid");
-    emailInput.classList.remove("border-invalid");
-  } else {
-    emailError.classList.add("display-block");
-    emailInput.classList.add("border-invalid");
-    e.preventDefault();
-  }
+// modal open
+const modalValid = () => {
+  modalContact.innerHTML = `
+     <div class="modal">
+     <header>
+      <h2>Contactez-moi</h2>
+      <img src="assets/icons/close.svg" onclick="closeModal()" />
+    </header>
+    <form
+      name="reserve"
+      action="photographer.html"
+      method="get"
+      id="formvalidation"
+    >
+      <div class="formData">
+        <label for="first">Prénom</label><br />
+        <input
+          class="text-control"
+          type="text"
+          id="first"
+          name="first"
+        /><br />
+        <p class="prenom-error style-error">
+          Veuillez entrer 2 caractères ou plus
+        </p>
+      </div>
+      <div class="formData">
+        <label for="last">Nom</label><br />
+        <input
+          class="text-control"
+          type="text"
+          id="last"
+          name="last"
+        /><br />
+        <p class="name-error style-error">
+          Veuillez entrer 2 caractères ou plus
+        </p>
+      </div>
+      <div class="formData">
+        <label for="email">E-mail</label><br />
+        <input
+          class="text-control"
+          type="text"
+          id="email"
+          name="email"
+        /><br />
+        <p class="message-adresse style-error">
+          veuillez entrer une adresse émail valide
+        </p>
+      </div>
+      <div class="formData">
+        <label for="message">Votre message</label><br />
+        <textarea
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <br />
+        <p class="message-error">
+          veuillez entrer un minimun de caractères
+        </p>
+      </div>
+      <button class="contact_button">Envoyer</button>
+    </form>
+  </div>`;
+};
+// creation du modele de validation 
+const formFunction = async () => {
+  await modalValid();
 
-// your message
-  if (yourMessage.value.length <= 4) {
-    messageError.classList.add("display-block");
-    yourMessage.classList.add("border-invalid");
-    e.preventDefault();
-  } else {
-    messageError.classList.remove("display-block");
-    yourMessage.classList.remove("border-invalid");
-    yourMessage.classList.add("border-valid");
-  }
-
-});
+  const formValidation = document.querySelector("#formvalidation");
+  console.log(formValidation);
+  // all Input
+  const prenomInput = document.querySelector("#first");
+  console.log(prenomInput);
+  const nameInput = document.querySelector("#last");
+  const emailInput = document.querySelector("#email");
+  const yourMessage = document.querySelector("#message");
+  // message error
+  const prenomError = document.querySelector(".prenom-error");
+  const nameError = document.querySelector(".name-error");
+  const emailError = document.querySelector(".message-adresse");
+  const messageError = document.querySelector(".message-error");
+  // regex
+  let regexmail =
+    /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+  // form -- Validation
+  formValidation.addEventListener("submit", (e) => {
+    // first name
+    if (prenomInput.value.length <= 1) {
+      prenomError.classList.add("display-block");
+      prenomInput.classList.add("border-invalid");
+      e.preventDefault();
+    } else {
+      prenomError.classList.remove("display-block");
+      prenomInput.classList.remove("border-invalid");
+      prenomInput.classList.add("border-valid");
+    }
+    // name
+    if (nameInput.value.length <= 1) {
+      nameError.classList.add("display-block");
+      nameInput.classList.add("border-invalid");
+      e.preventDefault();
+    } else {
+      nameError.classList.remove("display-block");
+      nameInput.classList.remove("border-invalid");
+      nameInput.classList.add("border-valid");
+    }
+    // email
+    if (emailInput.value.match(regexmail)) {
+      emailError.classList.remove("display-block");
+      emailInput.classList.add("border-valid");
+      emailInput.classList.remove("border-invalid");
+    } else {
+      emailError.classList.add("display-block");
+      emailInput.classList.add("border-invalid");
+      e.preventDefault();
+    }
+    // your message
+    if (yourMessage.value.length <= 4) {
+      messageError.classList.add("display-block");
+      yourMessage.classList.add("border-invalid");
+      e.preventDefault();
+    } else {
+      messageError.classList.remove("display-block");
+      yourMessage.classList.remove("border-invalid");
+      yourMessage.classList.add("border-valid");
+    }
+    if(prenomInput.value.length >= 2 && nameInput.value.length >= 2  && emailInput.value.match(regexmail) && yourMessage.value.length >= 4){
+      console.log("good");
+      location.reload();
+      e.preventDefault();
+    }
+    e.preventDefault();  
+  });
+};
+formFunction();
